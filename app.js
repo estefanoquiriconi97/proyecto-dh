a8e69057fb03a33f3e3a1dc1ac1a6db531e441dd
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-11-09 17:07:06 -0300

    Registro terminado

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"))
+})
